e0ce4c593f4f47ccd7b3884b30f365040eef0096
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 01:09:49 -0300

    :sparkles: feat: caso de uso de delete, usando gateway como roteador

---
+++ application/src/main/java/br/com/fullcycle/application/category/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,20 @@
+package br.com.fullcycle.application.category.delete;
+
+import br.com.fullcycle.domain.category.CategoryGateway;
+import br.com.fullcycle.domain.category.CategoryID;
+
+import java.util.Objects;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public void execute(final String anIn) {
+        this.categoryGateway.deleteById(CategoryID.from(anIn));
+    }
+}
