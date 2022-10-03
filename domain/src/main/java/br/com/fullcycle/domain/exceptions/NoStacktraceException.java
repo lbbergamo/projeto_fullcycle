4d9b4de11654d077a95378b3029280a841904e89
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 00:57:42 -0300

    :sparkles: feat: personalizando a geração de error

---
+++ domain/src/main/java/br/com/fullcycle/domain/exceptions/NoStacktraceException.java
@@ -0,0 +1,13 @@
+package br.com.fullcycle.domain.exceptions;
+
+public class NoStacktraceException extends RuntimeException {
+
+    public NoStacktraceException(final String message) {
+        super(message, null);
+    }
+
+    public NoStacktraceException(final String message, final Throwable cause) {
+        super(message, cause, true, false);
+    }
+}
+
