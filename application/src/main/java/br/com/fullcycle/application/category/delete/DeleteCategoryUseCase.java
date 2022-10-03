f30f01d014c9fd0451457db08f30357467e5b3e2
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 01:09:25 -0300

    :sparkles: feat: classe abastracta para o caso de uso de delete

---
+++ application/src/main/java/br/com/fullcycle/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package br.com.fullcycle.application.category.delete;
+
+import br.com.fullcycle.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}
