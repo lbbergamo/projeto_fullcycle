f077fb249a49d3988ec4ea15b923a6cef538a8b0
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 01:07:58 -0300

    :sparkles: feat: seguindo o factory de command utilizando record para criação de categoria

---
+++ application/src/main/java/br/com/fullcycle/application/category/create/CreateCategoryCommand.java
@@ -0,0 +1,14 @@
+package br.com.fullcycle.application.category.create;
+
+public record CreateCategoryCommand(String name,
+                                    String description,
+                                    boolean isActive) {
+
+    public static CreateCategoryCommand with(
+            final String aName,
+            final String aDescription,
+            final boolean isActive
+    ) {
+        return new CreateCategoryCommand(aName, aDescription, isActive);
+    }
+}
