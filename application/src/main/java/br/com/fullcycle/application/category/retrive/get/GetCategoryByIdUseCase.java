c88b8fbb8786d005751cd170c1f53520428764ce
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 01:12:27 -0300

    :sparkles: feat: classe base do category para busca por id

---
+++ application/src/main/java/br/com/fullcycle/application/category/retrive/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,6 @@
+package br.com.fullcycle.application.category.retrive.get;
+
+import br.com.fullcycle.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {
+}
