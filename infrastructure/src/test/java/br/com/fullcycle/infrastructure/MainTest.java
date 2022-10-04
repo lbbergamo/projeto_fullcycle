aa28b20c7eb05e9d5e3adc4b2ce705c06202dc2c
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 23:45:05 -0300

    :white_check_mark: test: adicicionando o teste utilizando a configuraão do banco de dados h2 para testes unitarios

---
+++ infrastructure/src/test/java/br/com/fullcycle/infrastructure/MainTest.java
@@ -0,0 +1,15 @@
+package br.com.fullcycle.infrastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test-integration");
+        Assertions.assertNotNull(new Main());
+        Main.main(new String[]{});
+    }
+}
