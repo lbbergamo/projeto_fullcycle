eee4e79da69fa438255f99389aa65d1c6eb74dba
Leandro Bergamo <leandro.bergamo@hotmail.com>
2022-10-03 23:41:50 -0300

    :sparkles: feat: criando a primeira entidade JPA de categoria.

---
+++ infrastructure/src/main/java/br/com/fullcycle/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package br.com.fullcycle.infrastructure.configuration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.fullcycle")
+public class WebServerConfig {
+}
